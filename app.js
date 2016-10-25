6e0c59daca9184595ac51e85240c969b9bdc2197
George Tony <gtony@suyati.com>
2016-10-25 17:22:42 +0530

    created class

--- app.js
+++ app.js
@@ -4,7 +4,19 @@ webpackJsonp([0],[
 
 	'use strict';
 	
+	Object.defineProperty(exports, '__esModule', {
+	  value: true
+	});
+	
+	var _createClass = (function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ('value' in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; })();
+	
+	var _get = function get(_x, _x2, _x3) { var _again = true; _function: while (_again) { var object = _x, property = _x2, receiver = _x3; _again = false; if (object === null) object = Function.prototype; var desc = Object.getOwnPropertyDescriptor(object, property); if (desc === undefined) { var parent = Object.getPrototypeOf(object); if (parent === null) { return undefined; } else { _x = parent; _x2 = property; _x3 = receiver; _again = true; desc = parent = undefined; continue _function; } } else if ('value' in desc) { return desc.value; } else { var getter = desc.get; if (getter === undefined) { return undefined; } return getter.call(receiver); } } };
+	
 	function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { 'default': obj }; }
+	
+	function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError('Cannot call a class as a function'); } }
+	
+	function _inherits(subClass, superClass) { if (typeof superClass !== 'function' && superClass !== null) { throw new TypeError('Super expression must either be null or a function, not ' + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
 	
 	var _react = __webpack_require__(1);
 	
@@ -14,11 +26,42 @@ webpackJsonp([0],[
 	
 	var _reactDom2 = _interopRequireDefault(_reactDom);
 	
-	_reactDom2['default'].render(_react2['default'].createElement(
-	  'p',
-	  null,
-	  ' hello world'
-	), document.getElementById('main'));
+	var HelloWorld = (function (_React$Component) {
+	  _inherits(HelloWorld, _React$Component);
+	
+	  function HelloWorld() {
+	    _classCallCheck(this, HelloWorld);
+	
+	    _get(Object.getPrototypeOf(HelloWorld.prototype), 'constructor', this).apply(this, arguments);
+	  }
+	
+	  _createClass(HelloWorld, [{
+	    key: 'render',
+	    value: function render() {
+	      console.log("HelloWorld");
+	      return _react2['default'].createElement(
+	        'div',
+	        null,
+	        _react2['default'].createElement(
+	          'p',
+	          null,
+	          ' Hello World'
+	        ),
+	        _react2['default'].createElement(
+	          'p',
+	          null,
+	          ' Is anyone there ?'
+	        )
+	      );
+	    }
+	  }]);
+	
+	  return HelloWorld;
+	})(_react2['default'].Component);
+	
+	exports.HelloWorld = HelloWorld;
+	
+	_reactDom2['default'].render(_react2['default'].createElement(HelloWorld, null), document.getElementById('main'));
 
 /***/ }
 ]);
--- lib/app.jsx
+++ lib/app.jsx
@@ -1,3 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-ReactDOM.render(<p> hello world</p>, document.getElementById('main'));
+export class HelloWorld extends React.Component {
+  render(){
+    console.log("HelloWorld")
+    return (
+      <div>
+        <p> Hello World</p>
+        <p> Is anyone there ?</p>
+      </div>
+    )
+  }
+}
+ReactDOM.render(<HelloWorld/>, document.getElementById('main'));
