aa246f6d6660d965eb6d940ccb9ca9b06920b343
George Tony <gtony@suyati.com>
2016-10-26 09:38:16 +0530

    comment

--- lib/app.jsx
+++ lib/app.jsx
@@ -7,6 +7,9 @@ export class HelloWorld extends React.Component {
       <div>
         <p> Hello World</p>
         <p> Is anyone there ?</p>
+        {/*
+          Put a input tag <input /> here and onChange function display Hello {name} equal to length of the name 
+          */}
       </div>
     )
   }
