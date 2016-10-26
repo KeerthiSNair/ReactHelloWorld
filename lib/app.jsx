49047139ce473a3b2f3afcaecd2b55617662c286
George Tony <gtony@suyati.com>
2016-10-26 09:40:15 +0530

    comment

--- lib/app.jsx
+++ lib/app.jsx
@@ -13,9 +13,12 @@ export class HelloWorld extends React.Component {
       <div>
         <p> Hello World</p>
         {this.state.name.length ? null : <p> Is anyone there ?</p> }
-        {/*  Input tag is used and onChange in value text is updated*/}
+        {/*  Input tag is used and onChange in value text is updated
+            Now Create a React Class {Multi} which print Hello {name}
+          */}
         <input value={this.state.name} onChange={ (e) => this.setState({name : e.target.value})} />
         {this.state.name.length ? <p>Hello {this.state.name}</p> : null }
+
       </div>
     )
   }
