d1f4105ad18e5bcc581f0a4b6ff7c9c4dadf9e39
George Tony <gtony@suyati.com>
2016-10-26 09:36:51 +0530

    comment

--- lib/app.jsx
+++ lib/app.jsx
@@ -1,3 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+/*
+Create A class HelloWorld and print hello world
+*/
 ReactDOM.render(<p> hello world</p>, document.getElementById('main'));
